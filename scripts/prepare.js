361eda007df5e8adc8ff81de43eff52bd3c8f4c9
Salil Sharma <150223667+salil-sharma-ar@users.noreply.github.com>
2025-09-18 15:19:38 +0530

    Added logs

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,7 +1,8 @@
 'use strict';
 const fs = require('fs');
 
-const languageServerVersion = '2.20.2-CODESCAN';
+const languageServerVersion = '2.20.3-CODESCAN';
+console.info("Copying language server jar version: " + languageServerVersion);
 
 if (!fs.existsSync('server')) {
   fs.mkdirSync('server');
@@ -15,3 +16,4 @@ function copy(url, dest) {
     fs.writeFileSync(dest, fs.readFileSync(url));
 }
 copy(`../sonarlint-language-server/target/sonarlint-language-server-${languageServerVersion}.jar`, 'server/sonarlint-ls.jar');
+console.info("Language server jar copied successfully!");
